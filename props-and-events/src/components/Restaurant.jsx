bdac8b621ae66464a37340566c7a76f924d8c97e
f3liz <felixchen14@gmail.com>
2025-04-08 11:49:31 -0700

    finished assignment for props

--- props-and-events/src/components/Restaurant.jsx
+++ props-and-events/src/components/Restaurant.jsx
@@ -1,8 +1,16 @@
-function Restaurant() {
+// renders each individual restaurants information
+function Restaurant(props) {
 
   return (
     <div className="restaurant">
-  
+      <img src={props.restaurant.image} alt={props.restaurant.name} />
+      <h2>{props.restaurant.name}</h2>
+      <ul>
+        <li>Address: {props.restaurant.address}</li>
+        <li>Phone: {props.restaurant.phone}</li>
+        <li>Type of food: {props.restaurant.cuisine}</li>
+        <li>⭐ rating: {props.restaurant.rating}</li>
+      </ul>
     </div>
   );
 }
--- props-and-events/src/components/RestaurantsContainer.jsx
+++ props-and-events/src/components/RestaurantsContainer.jsx
@@ -1,7 +1,15 @@
+// container for each restaurant and passes the data for each one to the next component
+import Restaurant from "./Restaurant";
+import { restaurants } from "../data/data.js"
+
 function RestaurantsContainer() {
   return (
     <div className="restaurantContainer">
-
+      {restaurants.map( restaurant => (
+        <Restaurant
+          restaurant={restaurant}
+        />
+      ))}
     </div>
   );
 }
